0a09b7fa754121a6fa382c88e27f59b4c7253ab8
Lucca Di Benedetto <luccadibe@gmail.com>
2024-06-19 12:02:21 +0200

    feature(functionRuntimeInterface) | Created interface and example hello function

---
+++ pkg/functionRuntimeInterface/functionRuntimeInterface.go
@@ -0,0 +1,123 @@
+package functionRuntimeInterface
+
+import (
+	"bufio"
+	"context"
+	"os"
+	"strings"
+	"time"
+
+	pb "github.com/3s-rg-codes/HyperFaaS/proto/function"
+	zerolog "github.com/rs/zerolog"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type handler func(context.Context, *Request) (*Response, error)
+
+type Request struct {
+	Data string
+	Id   string
+}
+
+type Response struct {
+	Data string
+	Id   string
+}
+
+type Function struct {
+	timeout  int
+	address  string
+	request  *Request
+	response *Response
+	id       string
+}
+
+func New(timeout int) *Function {
+	return &Function{
+		timeout:  timeout,
+		address:  "localhost:50052",
+		request:  &Request{},
+		response: &Response{},
+		id:       getID(),
+	}
+}
+
+func (f *Function) Ready(handler handler) {
+
+	//Set up logging
+	log := configLog("logs", "fn.log")
+
+	connection, err := grpc.NewClient(f.address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	log.Debug().Msgf("Connected to the worker: %v", f.address)
+
+	defer connection.Close()
+
+	if err != nil {
+		log.Error().Msgf("failed to connect: %v", err)
+	}
+
+	c := pb.NewFunctionServiceClient(connection)
+
+	//Set the id in the response to the id of the container
+	f.response.Id = f.id
+
+	for {
+		ctx, cancel := context.WithTimeout(context.Background(), 120*time.Second)
+		defer cancel()
+		//We ask for a new request whilst sending the response of the previous one
+		p, err := c.Ready(ctx, &pb.Payload{Data: f.response.Data, Id: f.response.Id})
+
+		f.request = &Request{p.Data, p.Id}
+
+		if err != nil {
+			log.Error().Msgf("failed to call: %v", err)
+			return
+		}
+
+		f.response, err = handler(ctx, f.request)
+
+		if err != nil {
+			log.Error().Msgf("Function failed: %v", err)
+			return
+		}
+
+	}
+
+}
+
+func configLog(logDir string, logFile string) *zerolog.Logger {
+	// Open the log file
+	file, err := os.OpenFile(logDir+"/"+logFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+
+	if err != nil {
+		panic(err)
+	}
+
+	log := zerolog.New(file).With().Timestamp().Logger()
+	return &log
+}
+
+func getID() string {
+	var id string
+	file, err := os.Open(".env")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, "CONTAINER_ID=") {
+			id = strings.TrimPrefix(line, "CONTAINER_ID=")
+			break
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+	return id
+}
