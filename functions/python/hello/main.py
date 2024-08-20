43ba4be17d27b3eb3b575972e05e1a3d21b65eae
jaquedon <johnson@campus.tu-berlin.de>
2024-08-20 14:09:10 +0200

    feature(python functions)| functions done,  functionRuntimeInterface cant import Payload

--- functions/python/hello/main.py
+++ functions/python/hello/main.py
@@ -1,2 +1,18 @@
-import
+import contextvars
+from urllib import request
 
+from pkg.pyFunctionRuntimeInterface.pyFuntionRuntimeInterface import Function, new, Response, Request
+
+
+def main():
+    function = new(120)
+
+    function.ready(handler)
+
+
+def handler(ctx, req: Request) -> Response:
+    response: Response = Response(
+        data="HELLO WORLD!",
+        id=req.id
+    )
+    return response
