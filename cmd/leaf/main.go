555e397a0f3e83275e96c297a6a0a04a302a1b83
linusgustafsson <linusgustafsson202@gmail.com>
2025-01-06 11:01:40 +0100

    Changed code to avoid merge issues later on.

--- cmd/leaf/main.go
+++ cmd/leaf/main.go
@@ -1,31 +1,6 @@
 package main
 
-import (
-	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
-	"os"
-)
-
 // start the leader
 
 // handle configuration
 // scrape intervals, batching of requests to the scheduler, etc.
-
-func main() {
-	// start the leader
-	log.Logger = log.Output(
-		zerolog.ConsoleWriter{
-			Out:     os.Stderr,
-			NoColor: false,
-		},
-	).With().Caller().Logger()
-
-	zerolog.DisableSampling(true)
-
-	log.Info().Msg("Starting leader")
-
-	for {
-		// handle configuration
-		// scrape intervals, batching of requests to the scheduler, etc.
-	}
-}
