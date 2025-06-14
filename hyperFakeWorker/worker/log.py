914907e3feeff5f2ddd20de911f13070d5c15b80
nikita.filenko <nikitafil2003@gmail.com>
2025-06-14 13:05:48 +0200

    Delete worker

--- hyperFakeWorker/worker/log.py
+++
@@ -1,5 +0,0 @@
-import logging
-
-logging.basicConfig(level=logging.INFO, format="[%(levelname)s] %(asctime)s - %(module)s: %(message)s")
-
-logger = logging.getLogger(None)
--- hyperFakeWorker/worker/utils/time.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-
-from google.protobuf import timestamp_pb2 as _timestamp_pb2
-
-def get_timestamp() -> _timestamp_pb2.Timestamp:
-    return _timestamp_pb2.Timestamp(seconds=datetime.now().timestamp())
