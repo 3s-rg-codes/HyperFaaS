de10be3a5e0fdeb4974d7e20d922d5c112d67d9b
Leo Wilhelm Lierse <lierseleow@gmail.com>
2025-06-22 11:35:12 +0200

    feat(onnx): implement basic model loading and inference functions and tests
    (cherry picked from commit 1b7948431cea501a3a4085e34fb9e57532f6842f by bennet.kleffgen@campus.tu-berlin.de)

--- hyperFakeWorker/tests/test_kvstore_client.py
+++ hyperFakeWorker/tests/test_kvstore_client.py
@@ -1,5 +1,8 @@
+import pytest
+
 from worker.kvstore.client import KVStoreClient
 
+@pytest.mark.external
 def test_client():
     c = KVStoreClient("127.0.0.1:8999")
     image = c.get_image("2e695271-8be9-4cdb-beb9-7480451753ce")
