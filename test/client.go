897efaca6708dae525bd5fab00250a810a39c517
Lucca Di Benedetto <64892925+luccadibe@users.noreply.github.com>
2025-12-02 11:14:41 +0100

    test: scale from zero e2e (#57)

--- test/client.go
+++ test/client.go
@@ -3,11 +3,45 @@
 package test
 
 import (
+	"context"
+	"fmt"
+
+	"github.com/3s-rg-codes/HyperFaaS/proto/common"
 	leafpb "github.com/3s-rg-codes/HyperFaaS/proto/leaf"
 	workerpb "github.com/3s-rg-codes/HyperFaaS/proto/worker"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
+
+// client interface for gRPC clients
+type client interface {
+	ScheduleCall(context.Context, *common.CallRequest, ...grpc.CallOption) (*common.CallResponse, error)
+}
+
+// GetHAProxyClient creates a gRPC client that connects to HAProxy
+func GetHAProxyClient(address string) client {
+	conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(fmt.Sprintf("Failed to connect to HAProxy: %v", err))
+	}
+	// We need to create a client that implements the ScheduleCall method
+	// Since HAProxy routes to leaves, we can use the leaf client interface
+	return &haproxyClient{conn: conn}
+}
+
+type haproxyClient struct {
+	conn *grpc.ClientConn
+}
+
+func (h *haproxyClient) ScheduleCall(ctx context.Context, req *common.CallRequest, opts ...grpc.CallOption) (*common.CallResponse, error) {
+	// Create a leaf client to make the call through HAProxy
+	leafClient := leafpb.NewLeafClient(h.conn)
+	return leafClient.ScheduleCall(ctx, req, opts...)
+}
+
+func (h *haproxyClient) Close() error {
+	return h.conn.Close()
+}
 
 func GetLeafClient(address string) (client leafpb.LeafClient, conn *grpc.ClientConn) {
 	conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
